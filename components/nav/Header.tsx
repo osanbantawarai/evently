16c80b5eb0904f4f5add866e1470ab4513c9e6a9
Osan Rai <osanbantawarai@gmail.com>
2025-04-23 17:19:56 +0545

    Footer made

--- components/nav/Header.tsx
+++ components/nav/Header.tsx
@@ -6,21 +6,29 @@ import { Button } from "../ui/button";
 import NavBar from "./NavBar";
 
 const Header = () => {
+  const navButtons = [
+    { label: "Home" },
+    { label: "Create Event" },
+    { label: "My Profile" },
+  ];
   return (
-    <header className="w-full border-b flex p-5 justify-between ">
+    <header className="w-full border-b flex justify-between items-center p-5">
       <div className="pl-10">
         <Link href="/">
           <Image src={assets.logo} alt="logo" width={128} height={38} />
         </Link>
       </div>
+      <div className="hidden sm:flex items-center justify-center gap-10 mr-20 font-medium">
+        {navButtons.map((item) => (
+          <button
+            key={item.label}
+            className="hover:text-[#634CF5] transition-colors duration-300 cursor-pointer"
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
       <div className="flex justify-end">
-        <Button
-          variant="default"
-          size="lg"
-          className="flex items-center px-6 bg-blue-600 hover:bg-blue-800"
-        >
-          Login
-        </Button>
         <NavBar />
       </div>
     </header>
--- components/nav/NavItems.tsx
+++ components/nav/NavItems.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const NavItems = () => {
+  const navButtons = [
+    { label: "Home" },
+    { label: "Create Event" },
+    { label: "My Profile" },
+  ];
   return (
-    <div>
-        Hello world
+    <div className="flex flex-col items-start ml-5 gap-3 font-medium">
+      {navButtons.map((item) => (
+        <button
+          key={item.label}
+          className="hover:text-[#634CF5] transition-colors duration-300"
+        >
+          {item.label}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
