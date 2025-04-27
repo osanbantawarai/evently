632e35218f14584e1df003664dc53c6cbfd609b8
Osan Rai <osanbantawarai@gmail.com>
2025-04-27 20:15:40 +0545

    final body finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,13 +9,17 @@ const page = () => {
       <div className="flex flex-col items-center sm:items-center sm:flex-row border-b gap-4 sm:gap-[10rem] pt-8 sm:pt-10 pb-10 sm:pb-10 sm:pr-10 bg-gray-50">
         <div className="flex flex-col gap-3 sm:gap-8 items-center sm:items-start sm:ml-30">
           <h1 className="text-4xl font-extrabold sm:text-6xl">
-            Host, Connect,<br /> Celebrate: Your Events,<br /> Our Platform!
+            Host, Connect,
+            <br /> Celebrate: Your Events,
+            <br /> Our Platform!
           </h1>
           <p className="text-md sm:text-xl mr-8">
-            Book and learn helpful tips from 3,168+ mentors in <br /> world-class
-            companies with out global community.
+            Book and learn helpful tips from 3,168+ mentors in <br />{" "}
+            world-class companies with out global community.
           </p>
-          <Button className="max-w-fit mr-60 sm:mr-0 bg-[#3467e6] hover:bg-[#4874e2] transition-colors duration-300 pt-5 pb-5 pl-8 pr-8 rounded-4xl">Explore Now</Button>
+          <Button className="sm:max-w-fit mr-60 sm:mr-0 bg-[#3467e6] hover:bg-[#4874e2] transition-colors duration-300 pt-5 pb-5 ml-50 sm:ml-0 pl-35 pr-35 sm:pl-8 sm:pr-8 rounded-4xl">
+            Explore Now
+          </Button>
         </div>
         <div>
           <Image
@@ -23,9 +27,19 @@ const page = () => {
             alt="hero"
             width={400}
             height={400}
-            className="w-100 sm:w-full pl-10 sm:pl-30"
+            className="w-110 sm:w-full pl-10 sm:pl-30"
           />
         </div>
+      </div>
+      <div className="ml-10 sm:ml-30 mt-8 mb-8 text-2xl sm:text-4xl font-extrabold">
+        <h2>
+          Trust by <br /> Thousands of Events
+        </h2>
+      </div>
+      <div className="ml-10 sm:ml-30">
+        <button>Search</button>
+        <button>Category</button>
+        <button>Filter</button>
       </div>
     </div>
   );
--- components/nav/Header.tsx
+++ components/nav/Header.tsx
@@ -1,15 +1,22 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "@/public/assets/assets";
 import NavBar from "./NavBar";
 
 const Header = () => {
+  const [activeButton, setActiveButton] = useState("Home")
   const navButtons = [
-    { label: "Home" },
+    { label: "Home"},
     { label: "Create Event" },
     { label: "My Profile" },
   ];
+
+  const handleClick = (label: string) => {
+    setActiveButton(label);
+  }
   return (
     <header className="sticky bg-white z-50 top-0 w-full border-b flex justify-between items-center pt-3 pb-3 sm:pt-5 sm:pb-5">
       <div className="pl-10 sm:pl-30">
@@ -20,8 +27,9 @@ const Header = () => {
       <div className="hidden sm:flex items-center justify-center gap-10 mr-20 font-medium">
         {navButtons.map((item) => (
           <button
+          onClick={()=>handleClick(item.label)}
             key={item.label}
-            className="hover:text-[#634CF5] transition-colors duration-300 cursor-pointer"
+            className={`hover:text-[#634CF5] transition-colors duration-300 cursor-pointer ${activeButton === item.label ? "text-[#634CF5]" : ""}`}
           >
             {item.label}
           </button>
