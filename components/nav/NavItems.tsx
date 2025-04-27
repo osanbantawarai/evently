c70a7199bbd547d5bea52dfea264ecd4be350329
Osan Rai <osanbantawarai@gmail.com>
2025-04-27 20:32:52 +0545

    final body finished

--- components/nav/NavItems.tsx
+++ components/nav/NavItems.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavItems = () => {
+  const [activeButton, setActiveButton] = useState("Home")
   const navButtons = [
     { label: "Home" },
     { label: "Create Event" },
     { label: "My Profile" },
   ];
+  const handleClick = (label:string) => {
+    setActiveButton(label)
+  }
   return (
     <div className="flex flex-col items-start ml-5 gap-3 font-medium">
       {navButtons.map((item) => (
         <button
+        onClick={()=>handleClick(item.label)}
           key={item.label}
-          className="hover:text-[#634CF5] transition-colors duration-300"
+          className={`hover:text-[#634CF5] transition-colors duration-300 ${activeButton == item.label ? "text-[#634CF5]" : "" }`}
         >
           {item.label}
         </button>
